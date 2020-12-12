89a2bb68ae669da3c9e7cb05b2f48cbf3b7c89a2
Mohannad Adham <mohannad.adm@gmail.com>
2020-12-12 05:49:39 +0100

    Module 2 - Drag and drop.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
 import sync from 'ol-hashed';
+import DragAndDrop from 'ol/interaction/DragAndDrop';
 
 
 const map = new Map({
@@ -23,6 +24,22 @@ const map = new Map({
     })
 });
 
+// add a vector source that will hold
+// the vector data added by the user
+const source = new VectorSource();
+
+// Create a vector layer and add it to the map
+const layer = new VectorLayer({
+    source: source
+});
+
+map.addLayer(layer);
+
+// create a drag and drop interaction
+map.addInteraction(new DragAndDrop({
+    source: source,
+    formatConstructors: [GeoJSON]
+}))
 // use the sync function to remember restore
 // the extent from the last session when reloading
 sync(map);
